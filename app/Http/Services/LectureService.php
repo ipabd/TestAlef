ad8aa442684313edaa91f41888c145408d05fef3
ipabd <101106448+ipabd@users.noreply.github.com>
2022-04-18 20:46:48 +0300

    Update LectureService.php

--- app/Http/Services/LectureService.php
+++ app/Http/Services/LectureService.php
@@ -1,54 +1,48 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: master
- * Date: 29.03.2022
- * Time: 18:24
- */
-
 namespace App\Http\Services;
+
 use App\Http\Requests\LectureRequest;
 use App\Models\Lecture;
+use Prophecy\Argument\Token\ObjectStateToken;
 
 
 class LectureService
 {
-    public function getLecture()
+protected Object $model;
+
+    public function __construct(Lecture $lecture)
     {
-        $resultLecture = [];
-        $lecture= Lecture::all();
-        $resultLecture[] = $lecture->map(function($elem) {
-            return ['name'=>$elem->name,'description'=>$elem->description];
-        });
-        return $resultLecture;
+        $this->model = $lecture;
     }
 
-    public function getShow($id)
+    public function getLecture(): array
     {
-        $resultLecture = [];
-        $lecture= Lecture::where('id',$id)->get();
-        $lecture->each(function ($item, $key) use (&$resultLecture) {
-            $resultLecture['lecture']=['name'=>$item->name,'description'=>$item->description];
-            $collection=$item->classst()->get();
-            foreach ($collection as $i=> $col) {
-                $resultLecture['classst'][]=$col->name;
-                $collectionst=$col->student()->get();
-                $st = $collectionst->map(function($elem) {
-                    return $elem->name;
-                });
-                foreach ($st->all() as $ii=> $s) {
-                    $resultLecture['student'][]=$s;
-                }
-            }
+        $resultLecture = (array)[];
+        $this->model::all()->map(function (Object $elem) use (&$resultLecture) {
+            $resultLecture[] = $elem->name;
         });
-        return $resultLecture;
+        return (array)$resultLecture;
+    }
+
+    public function getShow(int $id): array
+    {
+        $resultLecture = (array)[];
+        $lecture = $this->model::findOrFail($id);
+        $lecture->classst()->get()->map(function (Object $elem) use (&$resultLecture) {
+            $resultLecture['class'][] = $elem->name;
+            $elem->student()->get()->map(function (Object $el) use (&$resultLecture) {
+                $resultLecture['sduden'][] = $el->name;
+            });
+        });
+        return (array)$resultLecture;
     }
 
 
-    public function save(LectureRequest $request,  Lecture $lecture)
+    public function save(LectureRequest $request, ?int $id = 0): Object
     {
+        $lecture = (!$id) ? new $this->model : $this->model::find($id);
         $lecture->fill($request->only($lecture->getFillable()));
         $lecture->save();
-        return $lecture;
+        return (Object)$lecture;
     }
 }
