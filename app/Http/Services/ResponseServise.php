06fafbb117c958c084ac66bef0438d15b5f08ee2
ipabd <101106448+ipabd@users.noreply.github.com>
2022-04-18 20:48:22 +0300

    Update ResponseServise.php

--- app/Http/Services/ResponseServise.php
+++ app/Http/Services/ResponseServise.php
@@ -1,14 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: note
- * Date: 15.11.2020
- * Time: 10:22
- */
 namespace App\Http\Services;
 class ResponseServise
 {
-    private static function responsePrams($status, $errors = [], $data = []) {
+    private static function responsePrams(bool $status, $errors = [], $data = [])
+    {
         return [
             'status' => $status,
             'errors' => (object)$errors,
@@ -16,16 +11,26 @@ class ResponseServise
         ];
     }
 
-    public static function sendJsonResponse($status, $code = 200, $errors = [], $data = []) {
+    public static function sendJsonResponse(bool $status, $code = 200, $errors = [], $data = [])
+    {
         return response()->json(
-            self::responsePrams($status, $errors, $data),$code);
+            self::responsePrams($status, $errors, $data),
+            $code
+        );
     }
 
-    public static function success($data = []) {
-        return self::sendJsonResponse(true, 200, [],$data);
+    public static function success($data = [])
+    {
+        return self::sendJsonResponse(true, 200, [], $data);
     }
 
-    public static function notFound($data = []) {
-        return self::sendJsonResponse(false, 404, [],[]);
+    public static function notFound($data = [])
+    {
+        return self::sendJsonResponse(false, 404, ['не найдено'], []);
+    }
+
+    public static function ErrorApi()
+    {
+        return self::sendJsonResponse(false, 500, ['ошибка'], []);
     }
 }
