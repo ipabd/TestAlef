b16d66c786e01b157e2085a54c25df03b96768ac
ipabd <101106448+ipabd@users.noreply.github.com>
2022-04-18 21:12:54 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -20,22 +20,21 @@ use \App\Http\Controllers\Api\LectureController;
 
 
 Route::get('/student', [StudentController::class, 'index']);
-Route::get('/student/{student}', [StudentController::class, 'show']);
+Route::get('/student/{id}', [StudentController::class, 'show']);
 Route::post('/student', [StudentController::class, 'store']);
-Route::put('/student/{student}', [StudentController::class, 'update']);
-Route::delete('/student/{student}', [StudentController::class, 'destroy']);
+Route::put('/student/{id}', [StudentController::class, 'update']);
+Route::delete('/student/{id}', [StudentController::class, 'destroy']);
 
 Route::get('/classst', [ClassstController::class, 'index']);
-Route::get('/classst/{classst}', [ClassstController::class, 'show']);
+Route::get('/classst/{id}', [ClassstController::class, 'show']);
 Route::post('/classst', [ClassstController::class, 'store']);
-Route::put('/classst/{classst}', [ClassstController::class, 'update']);
-Route::delete('/classst/{classst}', [ClassstController::class, 'destroy']);
+Route::put('/classst/{id}', [ClassstController::class, 'update']);
+Route::delete('/classst/{id}', [ClassstController::class, 'destroy']);
 
 Route::get('/lecture', [LectureController::class, 'index']);
-Route::get('/lecture/{lecture}', [LectureController::class, 'show']);
+Route::get('/lecture/{id}', [LectureController::class, 'show']);
 Route::post('/lecture', [LectureController::class, 'store']);
-Route::put('/lecture/{lecture}', [LectureController::class, 'update']);
-Route::delete('/lecture/{lecture}', [LectureController::class, 'destroy']);
-
+Route::put('/lecture/{id}', [LectureController::class, 'update']);
+Route::delete('/lecture/{id}', [LectureController::class, 'destroy']);
 
 
